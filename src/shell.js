361cc99ce7115ceb312ed63272f538f43ee65dd5
Vamsee M Kamabathula <vamsee.mohan@gmail.com>
2016-05-25 10:06:12 +0530

    refactored code to different folders

--- src/shell.js
+++ src/shell.js
@@ -2,4 +2,28 @@ export class Shell {
 	constructor(){
 
 	}
+
+    configureRouter(config, router){
+        this.router = router;
+        // the title is displayed in the browser title bar
+        config.title = 'This is the shell!';
+        config.map([
+            {
+                route: ['', 'events'],
+                moduleId: 'events/events', name: 'Events',
+                title: 'Events listing', nav: true
+            },
+            {
+                route: ['sponsors'],
+                moduleId: 'sponsors/sponsors'
+            }, {
+                route: 'jobs', moduleId: 'jobs/jobs'
+                , title: 'Jobs listing', nav: true
+            },  {
+                route: 'discussion', moduleId: 'discussion/discussion'
+                , title: 'Discussion', nav: true
+            }
+        ]);
+
+    }
 }
