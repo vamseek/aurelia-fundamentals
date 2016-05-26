b00445c652d8cacc8ad9fb4d38699eb14df4502d
Vamsee M Kamabathula <vamsee.mohan@gmail.com>
2016-05-26 12:02:00 +0530

    added getEvents and getEvent implementation

--- src/services/dataRepository.js
+++ src/services/dataRepository.js
@@ -1,6 +1,30 @@
-import {eventsData} from 'eventsData';
+import {eventsData} from 'services/eventsData';
 import moment from 'moment';
 
 export class DataRepository {
+    constructor(){
+    }
 
+    getEvents(){
+        var promise = new Promise((resolve, reject) => {
+            if(!this.events){
+                setTimeout(_ => {
+                    this.events = eventsData;
+                    this.events.forEach(item => {
+                        var dateTime = moment(item.dateTime)
+                            .format("MM/DD/YYYY HH:mm");
+                        item.dateTime = dateTime;
+                    });
+                    resolve(this.events);
+                }, 2000);
+            } else {
+                resolve(this.events);
+            }
+        });
+        return promise;
+    }
+
+    getEvent(eventId){
+        return this.events.find(item => item.id == eventId);
+    }
 }
--- src/services/eventsData.js
+++ src/services/eventsData.js
@@ -1,17 +1,23 @@
 export var eventsData = [
     {
-        id: 124,
+        'id': 124,
         'description': 'REST is an architectural style for building HTTP Web API services',
-        'speaker': 'Vamsee Mohan Kamabathula'
+        'title': 'Designing RESTful services with ASP.NET WebAPI',
+        'speaker': 'Vamsee Mohan Kamabathula',
+        'dateTime': '2016-06-02T10:00:00.000Z'
     },
     {
-        id: 125,
+        'id': 125,
         'description': 'Introduction to Aurelia',
-        'speaker': 'Rob Eisenberg'
+        'title': 'Aurelia fundamentals',
+        'speaker': 'Rob Eisenberg',
+        'dateTime': '2016-06-02T11:00:00.000Z'
     },
     {
-        id: 136,
+        'id': 136,
         'description': 'High productivity',
-        'speaker': 'Robin Sharma'
+        'title': 'How to be your best!',
+        'speaker': 'Robin Sharma',
+        'dateTime': '2016-06-03T04:00:00.000Z'
     }
 ];
