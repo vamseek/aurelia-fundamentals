d1287b21378f77d4603445893df7d27f0e518ee0
Vamsee M Kamabathula <vamsee.mohan@gmail.com>
2016-05-27 08:50:02 +0530

    implemented discussion

--- src/discussion/discussion.js
+++ src/discussion/discussion.js
@@ -1,4 +1,36 @@
+function getDiscussionInput(){
+    // fake data access
+    return "";
+}
+
+function cloneObject(obj){
+    return JSON.parse(JSON.stringify(obj));
+}
+
 export class Discussion{
     constructor(){
+    }
+
+    activate(params, routeConfig, navigationInstruction){
+        this.discussionInput = getDiscussionInput();
+        this.originalInput = cloneObject(this.discussionInput);
+    }
+
+    save(){
+        // simulate save
+        this.originalInput = cloneObject(this.discussionInput);
+    }
+
+    canDeactivate(){
+        if(JSON.stringify(cloneObject(this.discussionInput)) !=
+           JSON.stringify(this.originalInput)){
+            if(confirm("Unsaved data, are you sure you want to navigate away?")){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
     }
 }
